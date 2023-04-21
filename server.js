3bdbce4c5a524072b57483ace0a0d024e26746dd
Your Name <you@example.com>
2023-04-20 18:13:42 -0600

    new changes

--- server.js
+++ server.js
@@ -19,23 +19,93 @@ app.use(cors());
 
 var router = express.Router();
 
-function getJSONObjectForMovieRequirement(req) {
-    var json = {
-        headers: "No headers",
-        key: process.env.UNIQUE_KEY,
-        body: "No body"
-    };
+// start here
+let Movie = require("./Movies");//makes the Movie obj work from the Movies.js exports line
 
-    if (req.body != null) {
-        json.body = req.body;
+router.route('/postjwt')
+    .post(authJwtController.isAuthenticated, function (req, res) {
+            console.log(req.body);
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                console.log("Content-Type: " + req.get('Content-Type'));
+                res = res.type(req.get('Content-Type'));
+            }
+            res.send(req.body);
+        }
+    );
+
+router.route('/users/:userId')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        var id = req.params.userId;
+        User.findById(id, function(err, user) {
+            if (err) res.send(err);
+
+            var userJson = JSON.stringify(user);
+            // return that user
+            res.json(user);
+        });
+    });
+router.route('/users')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        User.find(function (err, users) {
+            if (err) res.send(err);
+            // return the users
+            res.json(users);
+        });
+    });
+//end here
+
+router.post('/signup', function(req, res) {
+    if (!req.body.username || !req.body.password) {
+        res.json({success: false, message: 'Please pass username and password.'});
     }
+    else {
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+        // save the user
+        user.save(function(err) {
+            if (err) {
+                // duplicate entry
+                if (err.code == 11000)
+                    return res.json({ success: false, message: 'A user with that username already exists. '});
+                else
+                    return res.send(err);
+            }
 
-    if (req.headers != null) {
-        json.headers = req.headers;
+            res.json({ success: true, message: 'User created!' });
+        });
     }
+});
 
-    return json;
-}
+router.post('/signin', function(req, res) {
+    var userNew = new User();
+    userNew.name = req.body.name;
+    userNew.username = req.body.username;
+    userNew.password = req.body.password;
+
+    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
+        if(err){
+            res.send(err);
+        }else if(user == null){
+            res.send("Authentication Failed");
+        }else{
+            user.comparePassword(userNew.password, function(isMatch){
+                if (isMatch) {
+                    var userToken = {id: user._id, username: user.username};
+                    var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                    res.json({success: true, token: 'JWT ' + token});
+                }
+                else {
+                    res.status(401).send({success: false, message: 'Authentication failed.'});
+                }
+            });
+        }
+
+    });
+});
+
 router.route('/movies')
     .post(authJwtController.isAuthenticated, function (req, res) {
         if(req.body.Actors.length < 3){
@@ -235,56 +305,7 @@ router.route('/reviews')
         });
     });
 
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, message: 'Please pass username and password.'});
-    }
-    else {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
-        // save the user
-        user.save(function(err) {
-            if (err) {
-                // duplicate entry
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists. '});
-                else
-                    return res.send(err);
-            }
 
-            res.json({ success: true, message: 'User created!' });
-        });
-    }
-});
-
-router.post('/signin', function(req, res) {
-    var userNew = new User();
-    userNew.name = req.body.name;
-    userNew.username = req.body.username;
-    userNew.password = req.body.password;
-
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if(err){
-            res.send(err);
-        }else if(user == null){
-            res.send("Authentication Failed");
-        }else{
-            user.comparePassword(userNew.password, function(isMatch){
-                if (isMatch) {
-                    var userToken = {id: user._id, username: user.username};
-                    var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                    res.json({success: true, token: 'JWT ' + token});
-                }
-                else {
-                    res.status(401).send({success: false, message: 'Authentication failed.'});
-                }
-            });
-        }
-
-    });
-});
 
 
 router.all('*', function(req, res) {
